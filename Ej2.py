67b336f4fca91c3fdb923764a0b8078602aeb88a
Edgar <91616284+eedgaar05@users.noreply.github.com>
2022-11-10 16:46:31 +0100

    Add files via upload

---
+++ Ej2.py
@@ -0,0 +1,10 @@
+#Ej2
+"""
+1.Declaración de variable C que corresponde a grados Celsius, esta variable contendrá un input() para recibir parámetros.
+2.Para la convertir los grados Celsius en Farenheid debemos realizar la siguiente operacion C*(9/5)+32 que alamacenaremos en otra variable.
+3.Por ultimo imprimimos la variable C por pantalla.
+"""
+
+C = int(input("Escribe un num:"));
+F = C*(9/5)+32;
+print(F);
